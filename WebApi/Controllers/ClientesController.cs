715c155132a6f1cbc6c5e5479f9b7fe70cd1c746
Ton DeVeros <TonDeveros@gmail.com>
2024-05-16 21:18:19 -0300

    convert to file scoped controllers

--- WebApi/Controllers/ClientesController.cs
+++ WebApi/Controllers/ClientesController.cs
@@ -6,223 +6,222 @@ using Application.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.WebEncoders.Testing;
 
-namespace WebAPI.Controllers
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ClientesController : WebapiControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ClientesController : WebapiControllerBase
+    string errorMsg = string.Empty;
+
+
+    [HttpGet]
+    public IActionResult GetAllClients()
     {
-        string errorMsg = string.Empty;
+        var result = new vmResult();
 
-
-        [HttpGet]
-        public IActionResult GetAllClients()
+        try
         {
-            var result = new vmResult();
 
-            try
+            var service = Provider.GetService<IClientesService>();
+
+
+            var ret = service.GetAllClients();
+            result.Data = ret;
+            if (ret.Count == 0)
             {
-
-                var service = Provider.GetService<IClientesService>();
-
-
-                var ret = service.GetAllClients();
-                result.Data = ret;
-                if (ret.Count == 0)
-                {
-                    result.FriendlyErrorMessage = "Nenhuma informação encontrada.";
-                }
-                return Ok(result);
+                result.FriendlyErrorMessage = "Nenhuma informação encontrada.";
             }
-            catch (Exception ex)
-            {
-                result.FriendlyErrorMessage = "Erro inesperado";
-                result.StackTrace = ex.Message + "/n" + ex.StackTrace;
-                return BadRequest(result);
-            }
+            return Ok(result);
         }
-
-        [HttpPost, Route("ListClientes")]
-        public IActionResult ListClientes(vmClientes filter)
+        catch (Exception ex)
         {
-            var result = new vmResult();
-
-            try
-            {
-                
-                var service = Provider.GetService<IClientesService>();
-
-                filter.DataDoAtendimento = filter.DataDoAtendimento.Value.Date;//isso faz com que o horario sempre seja 00:00:00
-
-                var ret = service.ListCliente(filter);
-                result.Data = ret;
-                if (ret.Count == 0)
-                {
-                    result.FriendlyErrorMessage = "Nenhuma informação encontrada.";
-                }
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                result.FriendlyErrorMessage = "Erro inesperado";
-                result.StackTrace = ex.Message + "/n" + ex.StackTrace;
-                return BadRequest(result);
-            }
+            result.FriendlyErrorMessage = "Erro inesperado";
+            result.StackTrace = ex.Message + "/n" + ex.StackTrace;
+            return BadRequest(result);
         }
-
-        [HttpGet, Route("GetOrcaments")]
-        public IActionResult GetOrcaments()
-        {
-            var result = new vmResult();
-
-            try
-            {
-
-                var service = Provider.GetService<IClientesService>();
-
-                var ret = service.ListOrcamentos();
-                result.Data = ret;
-                if (ret.Count == 0)
-                {
-                    result.FriendlyErrorMessage = "Nenhuma informação encontrada.";
-                }
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                result.FriendlyErrorMessage = "Erro inesperado";
-                result.StackTrace = ex.Message + "/n" + ex.StackTrace;
-                return BadRequest(result);
-            }
-        }
-
-
-
-
-        [HttpPost]
-        [Route("AddCliente")]
-        public IActionResult AddCliente(vmClientes insCliente)
-        {
-            var result = new vmResult();
-
-            try
-            {
-                var service = Provider.GetService<IClientesService>();
-                insCliente.DataDoAtendimento = insCliente.DataDoAtendimento.Value.Date;//isso faz com que o horario sempre seja 00:00:00
-
-
-
-                bool isValid = Validate(insCliente);
-
-                if (isValid)
-                {
-                    result.Data = service.InsertCliente(insCliente);
-                    return Ok(result);
-                }
-                else
-                {
-                    result.FriendlyErrorMessage = errorMsg;
-                    return BadRequest(result);
-                }
-            }
-            catch (Exception ex)
-            {
-              
-
-                result.FriendlyErrorMessage = "Erro inesperado";
-                result.StackTrace = ex.Message + "/n" + ex.StackTrace;
-                return BadRequest(result);
-            }
-        }
-
-        [HttpPut]
-        [Route("UpdateCliente")]
-        public IActionResult UpdateCliente(vmClientes updCliente)
-        {
-            var result = new vmResult();
-
-            try
-            {
-                var service = Provider.GetService<IClientesService>();
-                updCliente.DataDoAtendimento = updCliente.DataDoAtendimento.Value.Date;//isso faz com que o horario sempre seja 00:00:00
-
-                bool isValid = Validate(updCliente);
-
-                
-
-                if(isValid)
-                {
-                    result.Data = service.UpdateCliente(updCliente);
-
-                    if (result.Data == null)
-                    {
-                         
-                        result.FriendlyErrorMessage = "Cliente não encontrado";
-                        return BadRequest(result);
-                    }
-                    return Accepted(result);
-                }
-                else
-                {
-                    result.FriendlyErrorMessage = errorMsg;
-                    return BadRequest(result);
-                }
-
-
-            }
-            catch (Exception ex)
-            {
-
-                result.FriendlyErrorMessage = "Erro inesperado";
-                result.StackTrace = ex.Message + "/n" + ex.StackTrace;
-                return BadRequest(result);
-            }
-        }
-
-        [HttpPut]
-        [Route("ConfirmPayment")]
-        public IActionResult ConfirmPayment(vmClientes cliente)
-        {
-            vmResult result = new vmResult();
-            try
-            {
-                var service = Provider.GetService<IClientesService>();
-
-                service.ConfirmPayment(cliente);
-
-                return Ok(cliente);
-            }
-
-            catch (Exception ex)
-            {
-
-                result.FriendlyErrorMessage = "Erro inesperado";
-                result.StackTrace = ex.Message + "/n" + ex.StackTrace;
-                return BadRequest(result);
-            }
-        }
-
-        private bool Validate(vmClientes cliente)
-        {
-            if (string.IsNullOrEmpty(cliente.NomeCliente) || cliente.NomeCliente.Length > 60)
-            {
-                errorMsg = "Por favor, insira um nome válido!";
-                return false;
-            }
-            if (string.IsNullOrEmpty(cliente.ClienteBairro) || cliente.ClienteBairro.Length > 60)
-            {
-                errorMsg = "Por favor, insira um bairro válido!";
-                return false;
-            }
-            if (string.IsNullOrEmpty(cliente.ClienteEndereco) || cliente.ClienteEndereco.Length > 60)
-            {
-                errorMsg = "Por favor, insira um endereco válido!";
-                return false;
-            }
-
-            return true;
-        }
-
-
     }
+
+    [HttpPost, Route("ListClientes")]
+    public IActionResult ListClientes(vmClientes filter)
+    {
+        var result = new vmResult();
+
+        try
+        {
+            
+            var service = Provider.GetService<IClientesService>();
+
+            filter.DataDoAtendimento = filter.DataDoAtendimento.Value.Date;//isso faz com que o horario sempre seja 00:00:00
+
+            var ret = service.ListCliente(filter);
+            result.Data = ret;
+            if (ret.Count == 0)
+            {
+                result.FriendlyErrorMessage = "Nenhuma informação encontrada.";
+            }
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            result.FriendlyErrorMessage = "Erro inesperado";
+            result.StackTrace = ex.Message + "/n" + ex.StackTrace;
+            return BadRequest(result);
+        }
+    }
+
+    [HttpGet, Route("GetOrcaments")]
+    public IActionResult GetOrcaments()
+    {
+        var result = new vmResult();
+
+        try
+        {
+
+            var service = Provider.GetService<IClientesService>();
+
+            var ret = service.ListOrcamentos();
+            result.Data = ret;
+            if (ret.Count == 0)
+            {
+                result.FriendlyErrorMessage = "Nenhuma informação encontrada.";
+            }
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            result.FriendlyErrorMessage = "Erro inesperado";
+            result.StackTrace = ex.Message + "/n" + ex.StackTrace;
+            return BadRequest(result);
+        }
+    }
+
+
+
+
+    [HttpPost]
+    [Route("AddCliente")]
+    public IActionResult AddCliente(vmClientes insCliente)
+    {
+        var result = new vmResult();
+
+        try
+        {
+            var service = Provider.GetService<IClientesService>();
+            insCliente.DataDoAtendimento = insCliente.DataDoAtendimento.Value.Date;//isso faz com que o horario sempre seja 00:00:00
+
+
+
+            bool isValid = Validate(insCliente);
+
+            if (isValid)
+            {
+                result.Data = service.InsertCliente(insCliente);
+                return Ok(result);
+            }
+            else
+            {
+                result.FriendlyErrorMessage = errorMsg;
+                return BadRequest(result);
+            }
+        }
+        catch (Exception ex)
+        {
+          
+
+            result.FriendlyErrorMessage = "Erro inesperado";
+            result.StackTrace = ex.Message + "/n" + ex.StackTrace;
+            return BadRequest(result);
+        }
+    }
+
+    [HttpPut]
+    [Route("UpdateCliente")]
+    public IActionResult UpdateCliente(vmClientes updCliente)
+    {
+        var result = new vmResult();
+
+        try
+        {
+            var service = Provider.GetService<IClientesService>();
+            updCliente.DataDoAtendimento = updCliente.DataDoAtendimento.Value.Date;//isso faz com que o horario sempre seja 00:00:00
+
+            bool isValid = Validate(updCliente);
+
+            
+
+            if(isValid)
+            {
+                result.Data = service.UpdateCliente(updCliente);
+
+                if (result.Data == null)
+                {
+                     
+                    result.FriendlyErrorMessage = "Cliente não encontrado";
+                    return BadRequest(result);
+                }
+                return Accepted(result);
+            }
+            else
+            {
+                result.FriendlyErrorMessage = errorMsg;
+                return BadRequest(result);
+            }
+
+
+        }
+        catch (Exception ex)
+        {
+
+            result.FriendlyErrorMessage = "Erro inesperado";
+            result.StackTrace = ex.Message + "/n" + ex.StackTrace;
+            return BadRequest(result);
+        }
+    }
+
+    [HttpPut]
+    [Route("ConfirmPayment")]
+    public IActionResult ConfirmPayment(vmClientes cliente)
+    {
+        vmResult result = new vmResult();
+        try
+        {
+            var service = Provider.GetService<IClientesService>();
+
+            service.ConfirmPayment(cliente);
+
+            return Ok(cliente);
+        }
+
+        catch (Exception ex)
+        {
+
+            result.FriendlyErrorMessage = "Erro inesperado";
+            result.StackTrace = ex.Message + "/n" + ex.StackTrace;
+            return BadRequest(result);
+        }
+    }
+
+    private bool Validate(vmClientes cliente)
+    {
+        if (string.IsNullOrEmpty(cliente.NomeCliente) || cliente.NomeCliente.Length > 60)
+        {
+            errorMsg = "Por favor, insira um nome válido!";
+            return false;
+        }
+        if (string.IsNullOrEmpty(cliente.ClienteBairro) || cliente.ClienteBairro.Length > 60)
+        {
+            errorMsg = "Por favor, insira um bairro válido!";
+            return false;
+        }
+        if (string.IsNullOrEmpty(cliente.ClienteEndereco) || cliente.ClienteEndereco.Length > 60)
+        {
+            errorMsg = "Por favor, insira um endereco válido!";
+            return false;
+        }
+
+        return true;
+    }
+
+
 }
--- WebApi/Controllers/UsuariosController.cs
+++ WebApi/Controllers/UsuariosController.cs
@@ -4,42 +4,42 @@ using Application.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
-namespace WebAPI.Controllers
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsuariosController : WebapiControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsuariosController : WebapiControllerBase
+    string errorMsg = string.Empty;
+
+    [HttpPost]
+    [Route("Login")]
+    public IActionResult Login(vmLogin user)
     {
-        string errorMsg = string.Empty;
+        var result = new vmResult();
 
-        [HttpPost]
-        [Route("Login")]
-        public IActionResult Login(vmLogin user)
+        try
         {
-            var result = new vmResult();
+            var service = Provider.GetService<IUsuariosService>();
+            result.Data = service.DoLogin(user);
 
-            try
+            if (result.Data == null)
             {
-                var service = Provider.GetService<IUsuariosService>();
-                result.Data = service.DoLogin(user);
-
-                if (result.Data == null)
-                {
-                    result.FriendlyErrorMessage = "Usuario não encontrado";
-                    return BadRequest(result);
-                }
-                return Ok();
-
-
-
-            }
-            catch (Exception ex)
-            {
-                
-                result.FriendlyErrorMessage = "Erro inesperado";
-                result.StackTrace = ex.Message + "/n" + ex.StackTrace;
+                result.FriendlyErrorMessage = "Usuario não encontrado";
                 return BadRequest(result);
             }
+            return Ok();
+
+
+
+        }
+        catch (Exception ex)
+        {
+
+            result.FriendlyErrorMessage = "Erro inesperado";
+            result.StackTrace = ex.Message + "/n" + ex.StackTrace;
+            return BadRequest(result);
         }
     }
 }
+
